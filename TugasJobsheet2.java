13d6e631fc82a8c64c3467a1a0b378d99b80352e
bimaadiwijaya <bimaadiwijaya83@gmail.com>
2024-09-05 10:41:56 +0700

    Biodata Bima

---
+++ TugasJobsheet2.java
@@ -0,0 +1,16 @@
+public class TugasJobsheet2 {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Bima Adiwijaya");
+        System.out.println("NIM      : 244107020022");
+        System.out.println("Kelas    : TI-1D");
+        System.out.println("No Absen : 07");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : Jl.Seruni no 38");
+        System.out.println("No HP    : 087877088963");
+        System.out.println("Hobi     : Bermain Game");
+    }
+}
