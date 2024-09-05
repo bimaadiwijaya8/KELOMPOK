b3a32ed26f674dc8a475135d3860010f1369b297
Sweper27 <rifqiariessaputra@gmail.com>
2024-09-05 10:55:51 +0700

    Biodata_Ari

---
+++ BiodataAri.java
@@ -0,0 +1,16 @@
+public class BiodataAri {
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Rifqi Aries Saputra");
+        System.out.println("NIM      : 244107020175");
+        System.out.println("Kelas    : TI-1D");
+        System.out.println("No Absen : 27");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : Jl.Karet no 27");
+        System.out.println("No HP    : 085156427506");
+        System.out.println("Hobi     : Berenang");
+    }
+}
