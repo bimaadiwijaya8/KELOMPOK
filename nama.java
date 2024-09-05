4503bb5718539793ae9bce76a9c1414c79627f4e
ANANTHA1628 <aryaputraferdiananta@gmail.com>
2024-09-05 11:06:03 +0700

    BIODATA NANTA

---
+++ nama.java
@@ -0,0 +1,19 @@
+/**
+ * tugas
+ */
+public class tugas {
+
+    public static void main(String[] args) {
+        System.out.println("-------------------------------------------------------");
+        System.out.println("--DATA MAHASISWA JURUSAN TEKNOLOGI INFORMASI POLINEMA--");        
+        System.out.println("-------------------------------------------------------");
+        System.out.print("NAMA     : ARYAPUTRA FERDIANANTA");
+        System.out.print("\nNIM      : 244107020127");
+        System.out.print("\nKELAS    : 1D");
+        System.out.print("\nNO.ABSEN : 06");
+        System.out.print("\nPRODI    : D-IV TEKNIK INFORMATIKA");
+        System.out.print("\nALAMAT   : JL.KEMANTREN 3 NO 55");
+        System.out.print("\nNO HP    : 08993356515");
+        System.out.print("\nHOBI     : MENDENGARKAN LAGU");
+    }
+}
